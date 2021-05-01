815b154b931241c51882803ffba5877d8e5c70df
Frank van Wattingen <f.vanwattingen@gmail.com>
2021-05-01 18:14:38 +0200

    Setting orientation now only applies to selected target

--- Assets/Scripts/Navigation/MoveToPoint.cs
+++ Assets/Scripts/Navigation/MoveToPoint.cs
@@ -13,17 +13,35 @@ public class MoveToPoint : MonoBehaviour
             return;
         }
 
-        if (!Input.GetMouseButtonDown(1))
-        {
-            return;
-        }
-
-        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
-        {
-            return;
-        }
-
         IAstarAI ai = selector.selectionState.selected.GetComponentInParent<IAstarAI>();
-        ai.destination = hit.point;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit downHit, 100))
+            {
+                return;
+            }
+
+            ai.destination = downHit.point;
+        }
+
+        if (!Input.GetMouseButtonUp(1))
+        {
+            return;
+        }
+
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit upHit, 100))
+        {
+            return;
+        }
+
+        Vector3 dirVector = upHit.point - ai.destination;
+        if (dirVector.sqrMagnitude < 1f)
+        {
+            return;
+        }
+
+        SetTurretRotation turret = selector.selectionState.selected.GetComponentInParent<SetTurretRotation>();
+        turret.SetOrientation(dirVector);
     }
 }
--- Assets/Scripts/Turret/SetTurretRotation.cs
+++ Assets/Scripts/Turret/SetTurretRotation.cs
@@ -13,29 +13,15 @@ public class SetTurretRotation : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 1f;
 
-    private Vector3 target = Vector3.forward;
+    private Vector3 target = Vector3.forward + Vector3.right;
 
-    private void Start()
+    public void SetOrientation(Vector3 to)
     {
-
+        target = to;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonUp(1))
-        {
-            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
-            {
-                return;
-            }
-
-            Vector3 dirVector = hit.point - ai.destination;
-            if (dirVector.sqrMagnitude > 1f)
-            {
-                target = dirVector;
-            }
-        }
-
         Vector3 newTurretDir = Vector3.RotateTowards(turretTransform.forward, target, rotationSpeed * Time.deltaTime, 0f);
         newTurretDir.y = turretTransform.forward.y;
         turretTransform.rotation = Quaternion.LookRotation(newTurretDir, Vector3.up);
