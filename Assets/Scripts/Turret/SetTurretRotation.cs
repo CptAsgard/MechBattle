0ea9284412eff78aab32778fa012213cb42dbd2f
Frank van Wattingen <f.vanwattingen@gmail.com>
2021-05-01 18:05:21 +0200

    Click to drag for directing view cone

--- Assets/Scripts/Turret/SetTurretRotation.cs
+++ Assets/Scripts/Turret/SetTurretRotation.cs
@@ -1,32 +1,43 @@
+using System.Numerics;
+using Pathfinding;
 using UnityEngine;
-using UnityEngine.AI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class SetTurretRotation : MonoBehaviour
 {
     [SerializeField]
-    private NavMeshAgent agent = null;
-    [SerializeField]
-    private Transform bodyTransform = null;
+    private AIPath ai = null;
     [SerializeField]
     private Transform turretTransform = null;
     [SerializeField]
     private float rotationSpeed = 1f;
 
+    private Vector3 target = Vector3.forward;
+
     private void Start()
     {
-        agent.updateRotation = false;
+
     }
 
     private void Update()
     {
-        Vector3 newBodyDir = Vector3.RotateTowards(bodyTransform.forward, Vector3.Normalize(agent.steeringTarget - bodyTransform.position), rotationSpeed * Time.deltaTime, 0f);
-        newBodyDir.y = bodyTransform.forward.y;
-        Debug.DrawRay(bodyTransform.position, newBodyDir, Color.red);
-        bodyTransform.rotation = Quaternion.LookRotation(newBodyDir, Vector3.up);
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+            {
+                return;
+            }
 
-        Vector3 newTurretDir = Vector3.RotateTowards(turretTransform.forward, Vector3.Normalize(agent.pathEndPosition - turretTransform.position), rotationSpeed * Time.deltaTime, 0f);
+            Vector3 dirVector = hit.point - ai.destination;
+            if (dirVector.sqrMagnitude > 1f)
+            {
+                target = dirVector;
+            }
+        }
+
+        Vector3 newTurretDir = Vector3.RotateTowards(turretTransform.forward, target, rotationSpeed * Time.deltaTime, 0f);
         newTurretDir.y = turretTransform.forward.y;
-        Debug.DrawRay(turretTransform.position, newTurretDir, Color.red);
         turretTransform.rotation = Quaternion.LookRotation(newTurretDir, Vector3.up);
     }
 }
