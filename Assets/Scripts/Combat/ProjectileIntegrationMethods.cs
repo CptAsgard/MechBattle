3de52cb919f4170ca2ee5fa72515de240fd3c6f1
Frank van Wattingen <f.vanwattingen@gmail.com>
2021-05-14 15:42:59 +0200

    Clean up checked code again

--- Assets/Scripts/Combat/ProjectileIntegrationMethods.cs
+++ Assets/Scripts/Combat/ProjectileIntegrationMethods.cs
@@ -4,39 +4,10 @@ public static class ProjectileIntegrationMethods
 {
     public static void HeunsNoExternalForces(float timeStep, Vector3 currentPosition, Vector3 currentVelocity, out Vector3 newPosition, out Vector3 newVelocity)
     {
-        //Init acceleration
-        //Gravity
-        Vector3 acceleartionFactorEuler = Physics.gravity;
-        Vector3 acceleartionFactorHeun = Physics.gravity;
+        Vector3 accFactor = Physics.gravity;
+        Vector3 newVelEuler = currentVelocity + timeStep * accFactor;
 
-
-        //Init velocity
-        //Current velocity
-        Vector3 velocityFactor = currentVelocity;
-        //Wind velocity
-        //velocityFactor += new Vector3(2f, 0f, 3f);
-
-
-        //
-        //Main algorithm
-        //
-        //Euler forward
-        Vector3 pos_E = currentPosition + timeStep * velocityFactor;
-
-        //acceleartionFactorEuler += CalculateDrag(currentVelocity);
-
-        Vector3 vel_E = currentVelocity + timeStep * acceleartionFactorEuler;
-
-
-        //Heuns method
-        Vector3 pos_H = currentPosition + timeStep * 0.5f * (velocityFactor + vel_E);
-
-        //acceleartionFactorHeun += CalculateDrag(vel_E);
-
-        Vector3 vel_H = currentVelocity + timeStep * 0.5f * (acceleartionFactorEuler + acceleartionFactorHeun);
-
-
-        newPosition = pos_H;
-        newVelocity = vel_H;
+        newVelocity = currentVelocity + timeStep * 0.5f * (accFactor + accFactor);
+        newPosition = currentPosition + timeStep * 0.5f * (currentVelocity + newVelEuler);
     }
 }
--- Assets/Scripts/Combat/ProjectileWeapon.cs
+++ Assets/Scripts/Combat/ProjectileWeapon.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileWeapon : Weapon
@@ -23,7 +22,6 @@ public class ProjectileWeapon : Weapon
         }
 
         Aim();
-        DrawTrajectoryPath();
     }
 
     public override void Fire()
@@ -56,6 +54,7 @@ public class ProjectileWeapon : Weapon
         }
 
         float angle = (float) (lowAngle ?? highAngle);
+
         Origin.LookAt(Target.Current);
         Origin.localEulerAngles = new Vector3(360f - angle, Origin.localEulerAngles.y, Origin.localEulerAngles.z);
         AimDirection = Origin.forward;
@@ -63,39 +62,22 @@ public class ProjectileWeapon : Weapon
 
     void CalculateAngleToHitTarget(out float? theta1, out float? theta2)
     {
-        //Initial speed
-        float v = weaponData.muzzleVelocity;
+        float velocity = weaponData.muzzleVelocity;
+        Vector3 targetVector = Target.Current.position - Origin.position;
+        float x = new Vector3(targetVector.x, 0, targetVector.z).magnitude;
+        float gravity = -Physics.gravity.y;
 
-        Vector3 targetVec = Target.Current.position - Origin.position;
+        float velocitySqr = velocity * velocity;
 
-        //Vertical distance
-        float y = targetVec.y;
-
-        //Reset y so we can get the horizontal distance x
-        targetVec.y = 0f;
-
-        //Horizontal distance
-        float x = targetVec.magnitude;
-
-        //Gravity
-        float g = Mathf.Abs(Physics.gravity.y);
-
-
-        //Calculate the angles
-        float vSqr = v * v;
-
-        float underTheRoot = (vSqr * vSqr) - g * (g * x * x + 2 * y * vSqr);
+        float underRoot = velocitySqr * velocitySqr - gravity * (gravity * x * x + 2 * targetVector.y * velocitySqr);
 
         //Check if we are within range
-        if (underTheRoot >= 0f)
+        if (underRoot >= 0f)
         {
-            float rightSide = Mathf.Sqrt(underTheRoot);
-
-            float top1 = vSqr + rightSide;
-            float top2 = vSqr - rightSide;
-
-            float bottom = g * x;
-
+            float rightSide = Mathf.Sqrt(underRoot);
+            float top1 = velocitySqr + rightSide;
+            float top2 = velocitySqr - rightSide;
+            float bottom = gravity * x;
             theta1 = Mathf.Atan2(top1, bottom) * Mathf.Rad2Deg;
             theta2 = Mathf.Atan2(top2, bottom) * Mathf.Rad2Deg;
         }
@@ -104,74 +86,5 @@ public class ProjectileWeapon : Weapon
             theta1 = null;
             theta2 = null;
         }
-    }
-
-
-
-    //Display the trajectory path with a line renderer
-    void DrawTrajectoryPath()
-    {
-        //Start values
-        Vector3 currentVel = Origin.forward * weaponData.muzzleVelocity;
-        Vector3 currentPos = Origin.position;
-
-        Vector3 newPos = Vector3.zero;
-        Vector3 newVel = Vector3.zero;
-
-        List<Vector3> bulletPositions = new List<Vector3>();
-
-        //Build the trajectory line
-        bulletPositions.Add(currentPos);
-
-        //I prefer to use a maxIterations instead of a while loop 
-        //so we always avoid stuck in infinite loop and have to restart Unity
-        //You might have to change this value depending on your values
-        int maxIterations = 10000;
-
-        for (int i = 0; i < maxIterations; i++)
-        {
-            //Calculate the bullets new position and new velocity
-            CurrentIntegrationMethod(Time.fixedDeltaTime, currentPos, currentVel, out newPos, out newVel);
-
-            //Set the new value to the current values
-            currentPos = newPos;
-            currentVel = newVel;
-
-            //Add the new position to the list with all positions
-            bulletPositions.Add(currentPos);
-
-            //The bullet has hit the ground because we assume 0 is ground height
-            //This assumes the bullet is fired from a position above 0 or the loop will stop immediately
-            if (currentPos.y < 0f)
-            {
-                break;
-            }
-
-            //A warning message that something might be wrong
-            if (i == maxIterations - 1)
-            {
-                Debug.Log("The bullet newer hit anything because we reached max iterations");
-            }
-        }
-
-
-        //Display the bullet positions with a line renderer
-        lineRenderer.positionCount = bulletPositions.Count;
-
-        lineRenderer.SetPositions(bulletPositions.ToArray());
-    }
-
-
-
-    //Choose which integration method you want to use to simulate how the bullet fly
-    public static void CurrentIntegrationMethod(float timeStep, Vector3 currentPos, Vector3 currentVel, out Vector3 newPos, out Vector3 newVel)
-    {
-        //IntegrationMethods.BackwardEuler(timeStep, currentPos, currentVel, out newPos, out newVel);
-
-        //IntegrationMethods.ForwardEuler(timeStep, currentPos, currentVel, out newPos, out newVel);
-
-        //IntegrationMethods.Heuns(timeStep, currentPos, currentVel, out newPos, out newVel);
-
-        ProjectileIntegrationMethods.HeunsNoExternalForces(timeStep, currentPos, currentVel, out newPos, out newVel);
     }
 }
