e6cb7d6336f2db5b3414869f8fa7bb8270d13fb7
Frank van Wattingen <f.vanwattingen@gmail.com>
2021-07-24 15:22:12 +0200

    fixed error on rotate camera

--- Assets/Scripts/Input/CameraMoveActions.cs
+++ Assets/Scripts/Input/CameraMoveActions.cs
@@ -8,29 +8,20 @@ public class CameraMoveActions : NetworkBehaviour
 {
     [SerializeField]
     private float scrollSpeed;
-
-    private float rotateInput;
+    
     private Vector2 moveInput;
 
     public override void OnStartClient()
     {
         InputActionMap actionMap = GetComponent<PlayerInput>().currentActionMap;
-        actionMap.FindAction("TurnCamera").performed += context => rotateInput = context.ReadValue<float>();
         actionMap.FindAction("MoveCamera").performed += context => moveInput = context.ReadValue<Vector2>();
         actionMap.FindAction("MoveCamera").canceled += context => moveInput = Vector2.zero;
 
         FocusFriendlyMech(0);
     }
  
-    public float GetAxisCustom(string axisName)
-    {
-        return rotateInput;
-    }
-
     private void Update()
     {
-        CinemachineCore.GetInputAxis = GetAxisCustom;
-
         FocusTarget.Instance.transform.localRotation = Quaternion.Euler(0, Camera.main.transform.localEulerAngles.y, 0);
         FocusTarget.Instance.transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * scrollSpeed * Time.deltaTime, Space.Self);
     }
